269df03a7c5a6d580335aad98c79ed3c2a1b91f6
Katharina Kain <a1057247@univie.ac.at>
2024-01-26 14:08:27 +0100

    updated definition of angular momentum variation; extended check for whole sample

--- DOGcosmoS/check_angmom_reliability.py
+++ DOGcosmoS/check_angmom_reliability.py
@@ -46,22 +46,35 @@ def angmom(xyz, vxyz, m, frac):
     L = L[:, :Nfrac]
     Ltot = np.sqrt(np.sum(np.power(np.sum(L, axis=1), 2)))
     Lhat = np.sum(L, axis=1) / Ltot
-    print('Lhat: ', Lhat)
     return Lhat
 
-
-sg = SWIFTGalaxy(
+Lhat_variation_sample =[]
+for i in sample_indices:
+        
+    sg = SWIFTGalaxy(
           snapshotfile,    
              Velociraptor(
              #velociraptor_filebase=halo_catalogue,
              velociraptor_files=catalogue_files,
-             halo_index=41,
+             halo_index=i,
              centre_type='minpot',
              extra_mask='bound_only',
          ),
     )          
           
+
+    Lhat_varying_massfrac = []
+
+    for frac in [0.1, 0.15, 0.2, 0.25, 0.3]:#, 0.4, 0.5]:#, 0.6, 0.7, 0.8, 0.9]:
           
-for frac in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
-          
-    Lhat = angmom(sg.gas.coordinates, sg.gas.velocities, sg.gas.atomic_hydrogen_masses, frac)
+        Lhat = angmom(sg.gas.coordinates, sg.gas.velocities, sg.gas.atomic_hydrogen_masses, frac)
+        Lhat_varying_massfrac.append(Lhat)
+
+    Lhat_variation = np.sqrt(np.mean(np.std(Lhat_varying_massfrac, axis=0)**2))
+    #Lhat_variation = np.std(Lhat_varying_massfrac, axis=0)
+    Lhat_variation_sample.append(Lhat_variation)
+    if Lhat_variation > 0.1:
+        print(i, Lhat_variation)
+
+#print(Lhat_variation_sample)
+#np.save(path_results+'Lhat_variation', Lhat_variation_sample)
