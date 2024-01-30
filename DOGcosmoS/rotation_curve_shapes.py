e91a3fd9aee284a3a45154322a65c006c24c26cd
Katharina Kain <a1057247@univie.ac.at>
2024-01-30 01:08:39 +0100

    restructured the code and fixed some bugs

--- DOGcosmoS/rotation_curve_shapes.py
+++ DOGcosmoS/rotation_curve_shapes.py
@@ -1,9 +1,10 @@
 import numpy as np
 import configparser
+from scipy.interpolate import splrep, splev
 from sample_selection import read_paths_from_config, select_sample
 from vcirc import vcirc_total
 
-def quantify_rotation_curve_shapes(vmax_min_sample=60, vmax_max_sample=120, save=False):
+def quantify_rotation_curve_shapes(vmax_min_sample=60, vmax_max_sample=120, save=None):
     """Calculates toration curve shape parameters for your galaxy sample.
     
     Arguments:
@@ -12,20 +13,17 @@ def quantify_rotation_curve_shapes(vmax_min_sample=60, vmax_max_sample=120, save
              save:            If True, an numpy array containing vmax and vfid of your sample is saved to your output path.
     
     """
-    sample_indices = select_sample(vmax_min_sample, vmax_max_sample, save=False)
+    sample_indices = select_sample(vmax_min_sample, vmax_max_sample, save=None)
     vmax_sample = np.zeros(len(sample_indices))
     rfid_sample = np.zeros(len(sample_indices))
     vfid_sample = np.zeros(len(sample_indices))
     eta_rot_sample = np.zeros(len(sample_indices))
 
     for i in range(len(sample_indices)):
-        halo_index = sample_indices[i]
-    
-        vcirc_profile = np.load(path_results+'vcirc_profiles/vcirc_r_profile'+str(index)+'.npy')
-        r = vcirc_profile[:,0]
-        vcirc = vcirc_profile[:,1]
         
-        r, vcirc = vcirc_total(halo_index, save=False)
+        halo_index = sample_indices[i]
+        
+        r, vcirc = vcirc_total(halo_index, save=None)
     
         vmax = np.max(vcirc)
         rfid = 2 * vmax/70   #/70km/s kpc
@@ -41,9 +39,9 @@ def quantify_rotation_curve_shapes(vmax_min_sample=60, vmax_max_sample=120, save
         vfid_sample[i] = vfid
         eta_rot_sample[i] = vfid/vmax
 
-    vmax_vfid = np.concatenate([vmax_sample[:,np.newaxis], v_fid_sample[:,np.newaxis]], axis=1)
+    vmax_vfid = np.concatenate([vmax_sample[:,np.newaxis], vfid_sample[:,np.newaxis]], axis=1)
     
-    if save == True:
+    if save is not None:
         snapshotfile, catalogue_files, output_path = read_paths_from_config(config_file='specify_your_paths.ini')
         np.save(output_path+'vmax_vfid.npy', vmax_vfid)
         
--- DOGcosmoS/vcirc.py
+++ DOGcosmoS/vcirc.py
@@ -1,8 +1,12 @@
 import numpy as np
 import unyt as U
+#from unyt import G
+import matplotlib as plt
 from velociraptor import load as load_catalogue
+from swiftgalaxy import SWIFTGalaxy, Velociraptor
+from sample_selection import read_paths_from_config
 
-def vcirc_particle_type(xyz, m, save=False):
+def vcirc_particle_type(xyz, m):
     """Calculates the circular velocity contributed by the particle type of interest (e.g. stars, gas or dark matter).
     
     Arguments:
@@ -19,17 +23,20 @@ def vcirc_particle_type(xyz, m, save=False):
     vcirc = np.sqrt(U.G * m_encl / r).to('km/s')   
     return vcirc, r, m
 
-def vcirc_total(halo_index, save=False):
+def vcirc_total(halo_index, save=None, plot=None):
     """Calculates the total circular velocity.
     
     Arguments:
              halo_index: Halo index from VELOCIRaptor. Should be a single index, not an array.
              save:       If True, circular velocity profile (r,vcirc) will be saved to your output path as numpy array. Default is False
+             plot:       If True, a plot showing the contribution of the circular velocity from the different particle types (i.e. stars, gas and dark matter) will be created and saved at your output path.
     """
+    snapshotfile, halo_catalogue_files, output_path = read_paths_from_config()
+    
     sg = SWIFTGalaxy(
            snapshotfile,    
              Velociraptor(
-             halo_catalogue,
+             velociraptor_files=halo_catalogue_files,
              halo_index=halo_index,
              centre_type='minpot',
              extra_mask='bound_only',
@@ -44,46 +51,42 @@ def vcirc_total(halo_index, save=False):
     m_gas = sg.gas.masses
     m_dm = sg.dark_matter.masses
     
-    vcirc_stars, r_stars, m_stars = vcirc_particle_type(xyz_stars, m_stars, save=False)
-    vcirc_gas, r_gas, m_gas = vcirc_particle_type(xyz_gas, m_gas, save=False)
-    vcirc_dm, r_dm, m_dm = vcirc_particle_type(xyz_dm, m_dm, save=False)
-        
-    r_baryonic = np.concatenate([r_stars, r_gas], axis=0)
-    m_baryonic = np.concatenate([m_stars, m_gas], axis=0)
-    rsort_bar = np.argsort(r_baryonic, kind="quicksort")
-    r_baryonic = r_baryonic[rsort_bar]*U.kpc                # reintroduce lost units
-    m_baryonic = m_baryonic[rsort_bar]*10000000000*U.Msun
-    m_encl_bar = np.cumsum(m_baryonic)
-    vcirc_baryonic = np.sqrt(U.G * m_encl_bar / r_baryonic).to('km/s')
-        
-    vcirc_bar_profile = np.concatenate([r_baryonic[:,np.newaxis], vcirc_baryonic[:,np.newaxis]], axis=1)
-    np.save(path_results+'vcirc_bar_profiles/vcirc_bar_r_profile'+str(index)+'.npy', vcirc_bar_profile)
+    vcirc_stars, r_stars, m_stars = vcirc_particle_type(xyz_stars, m_stars)
+    vcirc_gas, r_gas, m_gas = vcirc_particle_type(xyz_gas, m_gas)
+    vcirc_dm, r_dm, m_dm = vcirc_particle_type(xyz_dm, m_dm)
 
     r_all = np.concatenate([r_stars, r_gas, r_dm[1:]], axis=0) # exclude innermost part because of peak behaviour
     m_all = np.concatenate([m_stars, m_gas, m_dm[1:]], axis=0)
     rsort_all = np.argsort(r_all, kind="quicksort")
-    r_all = r_all[rsort_all]*U.kpc                # reintroduce lost units
-    m_all = m_all[rsort_all]*10000000000*U.Msun
+    r_all = r_all[rsort_all] *U.kpc                # reintroduce lost units
+    m_all = m_all[rsort_all] *10000000000*U.Msun
     m_encl_all = np.cumsum(m_all)
     vcirc_all = np.sqrt(U.G * m_encl_all / r_all).to('km/s')
     
+    if save is not None:
+        vcirc_profile = np.concatenate([r_all[:,np.newaxis], vcirc_all[:,np.newaxis]], axis=1)
+        np.save(output_path+'r_vcirc_profile'+str(halo_index)+'.npy', vcirc_profile)
+    
+    if plot is not None:   
+        R = sg.halo_finder.radii.r_halfmass.to('kpc').value
+
+        fig = plt.figure()
+        plt.plot(r_stars, vcirc_stars, color='tab:orange')
+        plt.plot(r_gas, vcirc_gas, color='tab:cyan')
+        plt.plot(r_dm[1:], vcirc_dm[1:], color='tab:purple') # exclude innermost part because of peak behaviour
+        plt.plot(r_all, vcirc_all, 'k')
+        plt.xlim([0,R])
+        plt.title('Circular velocity')
+        plt.xlabel('r [kpc]')
+        plt.ylabel('v_circ [km/s]')
+        plt.legend(['stars', 'gas', 'dark matter', 'total'])
+
+        fig.savefig(output_path+'vcirc_'+str(halo_index)+'.png')
+        plt.close()
+        
     return r_all, vcirc_all
     
-        #vcirc_profile = np.concatenate([r_all[:,np.newaxis], vcirc_all[:,np.newaxis]], axis=1)
-        #np.save(path_results+'vcirc_profiles/vcirc_r_profile'+str(index)+'.npy', vcirc_profile)
+       
 
 
-    #R = sg.halo_finder.radii.r_halfmass.to('kpc').value
 
-       # fig = plt.figure()
-       # plt.plot(r_stars, vcirc_stars, color='tab:orange')
-       # plt.plot(r_gas, vcirc_gas, color='tab:cyan')
-       # plt.plot(r_dm[1:], vcirc_dm[1:], color='tab:purple') # exclude innermost part because of peak behaviour
-       # plt.plot(r_all, vcirc_all, 'k')
-       # plt.xlim([0,R])
-       # plt.title('Circular velocity')
-       # plt.xlabel('r [kpc]')
-       # plt.ylabel('v_circ [km/s]')
-       # plt.legend(['stars', 'gas', 'dark matter', 'total'])
-
-       # fig.savefig(path_plots+'vcirc_'+str(index)+'.png')
