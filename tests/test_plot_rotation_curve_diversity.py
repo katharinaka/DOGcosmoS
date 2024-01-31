90e7b3ed3843ec403f963f4c1ffc9edb78d293b7
Katharina Kain <a1057247@univie.ac.at>
2024-01-31 19:54:56 +0100

    insterted directional statement in test function and corrected paths in test functions

--- tests/test_plot_rotation_curve_diversity.py
+++ tests/test_plot_rotation_curve_diversity.py
@@ -1,16 +1,16 @@
 import sys
-sys.path.append('/Users/puk/OSS_development/DOGcosmoS')
+sys.path.append('/Users/puk/OSS_development/DOGcosmoS/DOGcosmoS')
 
 import numpy as np
 import configparser
 import matplotlib
-from DOGcosmoS.sample_selection import select_sample, read_paths_from_config
-from DOGcosmoS.rotation_curve_shapes import quantify_rotation_curve_shapes
-from DOGcosmoS.vcirc import vcirc_total, vcirc_particle_type
-from DOGcosmoS.plots.plot_rotation_curve_diversity import plot_vfid_vmax
+from sample_selection import select_sample, read_paths_from_config
+from rotation_curve_shapes import quantify_rotation_curve_shapes
+from vcirc import vcirc_total, vcirc_particle_type
+from plot_rotation_curve_diversity import plot_vfid_vmax
 
 def test_plot_vfid_vmax():
 
-    plot_vfid_vmax() 
+    plot_vfid_vmax(200, 250) 
 
     assert True
--- tests/test_sample_selection.py
+++ tests/test_sample_selection.py
@@ -9,10 +9,10 @@ from sample_selection import select_sample, read_paths_from_config
 def test_read_paths():
     snapshotfile, halo_catalogue_file_base, halo_catalogue_files, output_path = read_paths_from_config(config_file='specify_your_paths.ini')
 
-
+    # insert your paths specified in specify_yout_paths.ini here to assert that they are read correctly.
     assert snapshotfile == '/Users/puk/Downloads/cosmo_volume_example.hdf5'
     assert halo_catalogue_files['properties'] == '/Users/puk/Downloads/cosmo_volume_example.properties'
-    assert output_path is not None
+    assert output_path == '/Users/puk/Test'
 
 
 def test_select_sample():
